c01bb043d5c9486e2bc5edcd113c26b2adf845a0
Bennett Friedman <Ben.friedman@ou.edu>
2020-01-29 12:39:47 -0600

    Coin toss 50/50 complete

---
+++ Coin/Coin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose Heads or Tails");
+            string input = Console.ReadLine();
+
+            var rand = new Random();
+
+            var value = rand.Next(0, 2);
+
+            string answer = "";
+
+            if (value == 1)
+            {
+                answer = (" heads");
+            }
+            else if (value == 0)
+            {
+                answer = (" tails");
+            }
+            if (input == answer)
+            {
+                Console.WriteLine("Congratulations, you WIN");
+            }
+            else if (input != answer)
+            {
+                Console.WriteLine("LOSER!!!");
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
